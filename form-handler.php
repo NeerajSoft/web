bfa3d3e8cd5207a00f46a5de63d27c9a4845b441
NeerajSoft <114489992+NeerajSoft@users.noreply.github.com>
2022-09-27 17:49:22 +0530

    web commit

---
+++ form-handler.php
@@ -0,0 +1,26 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_form ='neerajkumary8542@neerajwebtacv.000webhostapp.com';
+
+$email_subject ='New Form Submission';
+
+$email_body ="User Name: $name.\n ".
+                "User Email: $visitor_email.\n ".
+                "User Subject: $subject.\n ".
+                "User Message: $message.\n ";
+
+         $to ='neerajkumary8542@gmail.com';
+         
+         $header = "From: $email_from \r\n";
+$header .="Reply-To: $visitor_email \r\n";
+
+
+mail($to,$email_subject,$email_body,$header);
+header("Location:  contact.html" )
+
+
+?>
